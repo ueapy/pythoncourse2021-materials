50ed75e6c325ce3367bff64328d9695008ef858c
Tiago Silva (Cefas) <tiago.silva@cefas.co.uk>
2021-05-26 17:44:42 +0100

    added example script for lesson 01

---
+++ scripts/calc_factorial.py
@@ -0,0 +1,11 @@
+
+n = int(input('Type a number, and its factorial will be printed: '))
+
+if n < 0:
+    raise ValueError('You must enter a non negative integer')
+
+factorial = 1
+for i in range(2, n + 1):
+    factorial = factorial*i
+
+print(factorial)
