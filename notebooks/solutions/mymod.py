ac7982b7b250a513e4cd3ec0ca5d5b58c1c93e7c
Tiago Silva (Cefas) <tiago.silva@cefas.co.uk>
2021-05-27 21:16:30 +0100

    replaced head flux for carbon emissions function

--- notebooks/solutions/mymod.py
+++ notebooks/solutions/mymod.py
@@ -1,18 +1,51 @@
 
-def calc_heat_flux(
-    u_atm, 
-    t_sea, 
-    rho=1.2, 
-    c_p=1004.5, 
-    c_h=1.2e-3, 
-    u_sea=1, 
-    t_atm=17):
-
-    # Calculates bulk heat flux from sea surface
-    q = rho * c_p * c_h * (u_atm - u_sea) * (t_sea - t_atm)
+def calc_co2e(dist,
+              returnf=False,
+              firstclass=False,
+              radforc=2.0,
+              ):
+    """
+    calculate equivalent carbon emissions from flights
     
-    return q
+    Parameters
+    ==========
+    dist - flight distance in km
+    
+    Optional inputs
+    ---------------
+    returnf - Return flight (default=False)
+    firstclass - First class flight (default=False)
+    radforc - radiative forcing factor (default=2.0)
+    
+    Returns
+    =======
+    CO2 equivalent emissions in kg
 
+    Emission factors (kg CO2e/pkm)
+    https://flygrn.com/blog/carbon-emission-factors-used-by-flygrn
+    
+    0.26744  < 700 km 
+    0.15845  700 – 2500
+    0.15119  > 2500 km 
+    """
+
+    if dist < 700:
+        emm_factor = 0.26744
+    elif dist > 2500:
+        emm_factor = 0.15119
+    else:
+        emm_factor = 0.15845
+        
+    co2e = emm_factor * dist
+
+    if returnf:
+        co2e = co2e * 2
+    if firstclass:
+        co2e = co2e * 2
+    
+    co2e = co2e / 2.0 * radforc
+    
+    return co2e
 
 
 def create_sst(region_name):
